f924988264373dfe073479f74d97a93355ce93e1
FelipeSBarros <FelipeSBarros@users.noreply.github.com>
2015-07-21 14:34:08 -0300

    small changes
    'small changes as cleaning row and col names from txt.

--- Dist_nodes_creation.R
+++ Dist_nodes_creation.R
@@ -5,7 +5,8 @@
 ######
 ##Script de preparação dos dados para uso no CONEFOR para um ou shapefiles de paisagem
 #####
-
+#limpando area de trabalho
+rm(list=ls())
 #carregando pacotes
 library(raster)
 library(reshape)
@@ -16,7 +17,8 @@ library(beepr)
 #####
 ##Defining path to all landscape shapefiles (after split)
 #####
-dsn_shapelist <- '/home/felipe/Projetos/Paraitinga/Conefor/Input_QGIS/Split/Oldgrowth'
+dsn_shapelist <- '/home/felipe/Projetos/Paraitinga/Conefor/Input_QGIS/Split/Oldgrowth/'
+
 #Creating list with all landscape shapefiles
 shapelist <- as.list(ogrListLayers(dsn_shapelist))#lista dos shapes existentes na pasta dsn
 #Creating some objects to be used guring the loop
@@ -38,7 +40,6 @@ distancia <- NULL
 system.time( #Monitorando o tempo de processamento do loop
 for( i in 1:length(shapes))
 {
-
 #Importando os shapes para uma lista
   shapes[[i]] <-  readOGR(dsn=dsn_shapelist, layer=shapelist[[i]])
 #Adding a new column and calculating the area of each polygon
@@ -46,7 +47,7 @@ for( i in 1:length(shapes))
 #creating nod matrix to have values inserted
   nodes[[i]]<-matrix(c(1:length(shapes[[i]]),shapes[[i]]$area),nrow=length(shapes[[i]]))
 #writing tables with the IDHEX in the name of the text file
-  write.table(nodes[[i]], file=sprintf("%s.nodes_oldgrowth",shapes[[i]]$HEXID[i]), row.names = F, col.names = F,    sep="\t", quote=F)
+  write.table(nodes[[i]], file=sprintf("%s.nodes_oldgrowth.txt",shapes[[i]]$HEXID[i]), row.names = F, col.names = F,    sep="\t", quote=F)
 #Calculating distances between each polygon
   dists[[i]]<-gDistance(shapes[[i]], shapes[[i]],byid=T)
   dists_melt[[i]] <- melt(dists[[i]])[melt(upper.tri(dists[[i]]))$value,]
@@ -66,6 +67,7 @@ for( i in 1:length(shapes))
   pesos2[[i]] <- (pesos[[i]][2:dim(pesos[[i]])[1],])
   distancia[[i]] <- cbind(pesos2[[i]],dists_melt[[i]][,3])
 #writting distance results in text files
-  write.table(distancia[[i]],file=sprintf("%s.dists_oldgorwth", shapes[[i]]$HEXID[i]), sep="\t", quote=F)
+  write.table(distancia[[i]],file=sprintf("%s.dists_oldgrowth.txt", shapes[[i]]$HEXID[i]),row.names=F, col.names=F, sep="\t", quote=F)
+    print(i)
 }, gcFirst = TRUE)
 ############
